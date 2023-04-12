7a207c7d4a2477a7453db9a87bc0d44409dc0f24
kumkumrathour <neharathour9669535194@gmail.com>
2023-04-12 17:31:29 +0530

    portfolio

---
+++ myportfolio/src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import image from '../Image/image.png';
+// import {MdOutlineKeyboardDoubleArrowRight}  from 'react-icons/md'
+const Home = () => {
+  return (
+    <div>
+
+        {/* <MdOutlineKeyboardDoubleArrowRight/> */}
+        {/* <img src={image} alt=""/> */}
+
+      <div>Home</div>
+    </div>
+  );
+}
+
+export default Home;
